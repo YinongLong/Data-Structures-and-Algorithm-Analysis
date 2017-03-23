7e55b17d7779e7cbad7eb017799d4f7ebe2a1f3e
YinongLong <pleasecallmegeek@gmail.com>
2017-03-23 17:15:04 +0800

    进行有权（非负）最短路径的实现

--- graph.py
+++ graph.py
@@ -6,6 +6,7 @@ Created on Wed Mar 22 08:25:14 2017
 """
 from __future__ import print_function
 
+from itertools import count
 from Queue import Queue
 
 class Graph(object):
@@ -15,7 +16,7 @@ class Graph(object):
     
     def __init__(self, edge_pairs):
         """
-        初始化图
+        初始化图，将图初始化为带权图，如果初始化时没有给出权值，则默认为1
         
         Parameters
         ----------
@@ -26,26 +27,30 @@ class Graph(object):
         # 存储顶点的名称
         self.names = []
         # 给定点分配编号
-        self._number = 0
+        self._counter = count()
         # 存储顶点名称和编号的对应关系
         self.name_coder = {}
         # 存储图的邻接表
         self._adjacencyList = []
         for edge in edge_pairs:
-            node1, node2 = edge
+            if len(edge) == 2:
+                node1, node2 = edge
+                weight = 1
+            else:
+                node1, node2, weight = edge
+                
             if node1 not in self.name_coder:
-                self.name_coder[node1] = self._number
+                self.name_coder[node1] = self._counter.next()
                 self.names.append(node1)
                 self._adjacencyList.append([])
-                self._number += 1
             if node2 not in self.name_coder:
-                self.name_coder[node2] = self._number
+                self.name_coder[node2] = self._counter.next()
                 self.names.append(node2)
                 self._adjacencyList.append([])
-                self._number += 1
+                
             num1 = self.name_coder[node1]
             num2 = self.name_coder[node2]
-            self._adjacencyList[num1].append(num2)
+            self._adjacencyList[num1].append((num2, weight))
         self.nums_vertex = len(self.names)
             
     def topologySort(self):
@@ -55,7 +60,7 @@ class Graph(object):
         # 首先计算每一个顶点的入度，这里会对每一条边进行遍历，因此时间复杂度为O(V+E)
         indegree = [0] * self.nums_vertex
         for nodes in self._adjacencyList:
-            for node in nodes:
+            for node, _ in nodes:
                 indegree[node] += 1
         # 将入度为0的定点放入队列，这里使用队列存储入度为0的顶点，相比于每次扫描一遍
         # 记录入度的数组的时间复杂度O(V^2)提高到了O(V+E)
@@ -68,7 +73,7 @@ class Graph(object):
         while not candidates.empty():
             index = candidates.get()
             topo_sort.append(self.names[index])
-            for node in self._adjacencyList[index]:
+            for node, _ in self._adjacencyList[index]:
                 indegree[node] -= 1
                 if indegree[node] == 0:
                     candidates.put(node)
@@ -95,7 +100,7 @@ class Graph(object):
         的每一个列表都对应的同一个引用，所有每一次对其中一个元素的赋值都会影响到其它所
         有的项！！
         """
-        records = [[None, None] for _ in xrange(self.nums_vertex)]
+        records = [[None, None] for _ in xrange(self.nums_vertex)] # 涉及对象的深浅拷贝问题
         vertex_num = self.name_coder[vertex_name]
         # 源点到自身的无权最短路径为0
        
@@ -106,7 +111,7 @@ class Graph(object):
         
         while not candidates.empty():
             temp_vertex = candidates.get()
-            for node in self._adjacencyList[temp_vertex]:
+            for node, _ in self._adjacencyList[temp_vertex]:
                 if records[node][0] == None:
                     records[node][0] = records[temp_vertex][0] + 1
                     records[node][1] = temp_vertex
@@ -121,23 +126,49 @@ class Graph(object):
                 index = nodes[index][1]
             return path
         
+        # 以递归的形式打印路径
+        def printPath(nodes, index):
+            if nodes[index][1] != None:
+                temp_path = printPath(nodes, nodes[index][1])
+                temp_path += " -> "
+                temp_path += self.names[index]
+                return temp_path
+            return self.names[index]
+        
         for index in xrange(self.nums_vertex):
-            path = getPath(records, index)
-            path += "->" + self.names[index]
+#            path = getPath(records, index)
+#            path += "->" + self.names[index]
+            path = printPath(records, index)
             print("The dist between Original vertex and %s is: %d" % (self.names[index], records[index][0]))
             print(path)
+            
+    def weightedShortPath(self, vertex_name):
+        """
+        计算指定源顶点到图中所有其它顶点的有权最短路径（无负权值）
+        
+        Patrameters
+        -----------
+        vertex_name: str
+            指定计算的源顶点
+        """
+        records = [[0, None] for _ in xrange(self.nums_vertex)]
+        num_vertex = self.names[vertex_name]
+        # 标记源顶点为已访问顶点
+        records[num_vertex][0] = 1
+        
+        
             
     def __str__(self):
         """
         打印图
         """
         print(" " * 3, "Vertex", " " * 6, "Adjacency Nodes")
-        for index in xrange(len(self._adjacencyList)):
+        for index in xrange(self.nums_vertex):
             if len(self._adjacencyList[index]) == 0:
                 print(" " * 3, self.names[index])
             else:
                 print(" " * 3,  self.names[index], " ", "-" * 4, " ", end="")
-                for node in self._adjacencyList[index]:
+                for node, _ in self._adjacencyList[index]:
                     print("  ", self.names[node], end="")
                 print()
         return ""

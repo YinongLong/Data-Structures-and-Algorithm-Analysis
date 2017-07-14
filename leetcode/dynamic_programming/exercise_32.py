258ba20fac6c7418b12ef71f41633d52e9d9af17
YinongLong <pleasecallmegeek@gmail.com>
2017-07-14 08:53:06 +0800

    32th dynamic programming problem, longest valid parentheses.

---
+++ leetcode/dynamic_programming/exercise_32.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+
+class Solution(object):
+
+    def longestValidParentheses(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        
+        pass

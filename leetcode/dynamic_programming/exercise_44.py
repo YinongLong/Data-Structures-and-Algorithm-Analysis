0aa43097efabb556d692e55c688e5541420591f6
YinongLong <pleasecallmegeek@gmail.com>
2017-07-16 00:07:45 +0800

    exercise 44

---
+++ leetcode/dynamic_programming/exercise_44.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+class Solution(object):
+
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+        pass

27203936c92c25b14f570b81fe461249ef703b09
YinongLong <pleasecallmegeek@gmail.com>
2017-07-17 22:40:55 +0800

    take two pointers to solve the exercise

--- leetcode/dynamic_programming/exercise_44.py
+++ leetcode/dynamic_programming/exercise_44.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 
+from collections import defaultdict
+
 class Solution(object):
 
     def isMatch(self, s, p):
@@ -9,4 +11,33 @@ class Solution(object):
         :type p: str
         :rtype: bool
         """
-        pass
+        s_len = len(s)
+        p_len = len(p)
+        p_idx = 0
+        s_idx = 0
+        star_idx = -1
+        match_idx = -1
+        while s_idx < s_len:
+            if (p_idx < p_len) and (p[p_idx] == '?' or p[p_idx] == s[s_idx]):
+                p_idx += 1
+                s_idx += 1
+            elif p_idx < p_len and p[p_idx] == '*':
+                star_idx = p_idx
+                p_idx += 1
+                match_idx = s_idx
+            elif star_idx != -1:
+                p_idx = star_idx + 1
+                match_idx += 1
+                s_idx = match_idx
+            else:
+                return False
+        while (p_idx < p_len) and (p[p_idx] == '*'):
+            p_idx += 1
+        return True if p_idx == p_len else False
+
+
+print('--- start ---')
+s = Solution()
+result = s.isMatch('abcdefg', 'a*de?*g')
+print('--- ending ---')
+print(result)

cb0602c4ebab86a40ff386d1f2207c58a2278a27
YinongLong <pleasecallmegeek@gmail.com>
2017-07-20 21:55:43 +0800

    update

---
+++ leetcode/misc/exercise_37.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+
+class Solution(object):
+
+    def solveSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: void Do not return anything, modify board in-place instead.
+        """
+        
+        pass
+    pass

27095f687ea010154ce25ae5957a32a01654465d
YinongLong <pleasecallmegeek@gmail.com>
2017-07-22 23:18:48 +0800

    update

--- leetcode/misc/exercise_37.py
+++ leetcode/misc/exercise_37.py
@@ -3,10 +3,35 @@ from __future__ import print_function
 
 from collections import defaultdict
 
-class Solution(object):
 
-    def get_location_info(self, i, j):
-        return ('r%d' % i, 'c%d' % j, 'b%d%d' % (i/3, j/3))
+class Node(object):
+
+    def __init__(self, p_i=None, p_j=None, p_item=None, p=None):
+        self.p_i = p_i
+        self.p_j = p_j
+        self.p_item = p_item
+        self.p = p
+        self.candidates = []
+
+    def generate_children(self, i, j, state):
+        r_info, c_info, b_info = get_location_info(i, j)
+        for item in map(str, range(1, 10)):
+            if not relationship(state[item], r_info, c_info, b_info):
+                self.candidates.append(Node(i, j, item, self))
+
+
+def get_location_info(i, j):
+    return ('r%d' % i, 'c%d' % j, 'b%d%d' % (i/3, j/3))
+
+
+def relationship(state_set, r_info, c_info, b_info):
+    if (r_info in state_set) or (c_info in state_set) or (b_info in state_set):
+        return True
+    else:
+        return False
+
+
+class Solution(object):
 
     def solveSudoku(self, board):
         """
@@ -17,33 +42,26 @@ class Solution(object):
 
         # 首先统计整个数独board的信息
         state = defaultdict(set)
+        locations = []
         for i in range(size):
             row = board[i]
             for j in range(size):
                 item = row[j]
                 if item == '.':
-                    continue
-                for info in self.get_location_info(i, j):
+                    locations.append((i, j))
+                for info in get_location_info(i, j):
                     state[item].add(info)
 
-        # 然后利用统计的数独board的信息来进行填数
-        i = j = 0
-        locations = []
-        while i < size:
-            numbers = map(str, range(1, 10))
-            temp_location = []
-            while j < size:
-                item = board[i][j]
-                if item == '.':
-                    temp_location.append((i, j))
-                else:
-                    numbers.remove(item)
-                j += 1
-            for location in temp_location:
-                r_id, c_id, b_id = self.get_location_info(*location)
-                for number in numbers:
-                    states_space = state[number]
-                    if (r_id not in states_space) and (c_id not in states_space) and (b_id not in states_space):
-                        pass
+        root = Node()
+        temp_node = root
+        while len(locations) > 0:
+            loc_i, loc_j = locations.pop()
+            temp_node.generate_children(loc_i, loc_j, state)
+            if len(temp_node.children) > 0:
+                child = temp_node.candidates.pop()
+                for info in get_location_info(child.p_i, child.p_j):
+                    state[child.p_item].add(info)
+                temp_node = child
+            else:
+                
                 pass
-            i += 1

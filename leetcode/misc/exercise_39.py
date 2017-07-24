0ab5d8e158e2930bca85c84cf60729da11c3cd8b
YinongLong <pleasecallmegeek@gmail.com>
2017-07-24 21:20:58 +0800

    original backtracking algorithm

--- leetcode/misc/exercise_39.py
+++ leetcode/misc/exercise_39.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 
+import copy
+
 class Node(object):
 
     def __init__(self, num, result_list, residual, parent):
@@ -19,14 +21,20 @@ class Node(object):
         self.result_list = result_list
         self.residual = residual
         self.parent = parent
-        self.children = None
+        self.children = []
 
-    def generate_children(self, nums_node):
+    def generate_children(self, nums):
         """
-        生成该节点的所有该子节点
-        :param nums_node: List[Node]
+        生成该节点的所有的合法（剩余值大于等于0）该子节点
+        :param nums: List[int]
         """
-        self.children = nums
+        for item in nums:
+            temp_residual = self.residual - item
+            if temp_residual < 0:
+                continue
+            temp_list = copy.copy(self.result_list)
+            temp_list.append(item)
+            self.children.append(Node(item, temp_list, temp_residual, self))
 
 
 class Solution(object):
@@ -38,9 +46,33 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         root = Node(None, [], target, None)
-
+        root.generate_children(candidates)
         temp_node = root
+        temp_result = []
         while temp_node is not None:
+            if len(temp_node.children) > 0:
+                # 首先进行一次检查，将所有的子节点剩余值为0的结果找到，即为一个合法值
+                finished = []
+                for idx, child in enumerate(temp_node.children):
+                    if child.residual == 0:
+                        temp_result.append(child.result_list)
+                        finished.append(idx)
+                temp_node.children = [temp_node.children[idx] for idx in range(len(temp_node.children)) if idx not in finished]
+                if len(temp_node.children) == 0:
+                    temp_node = temp_node.parent
+                else:  # 说明还有节点的值剩余，因此继续将搜索树进行扩展
+                    temp_node = temp_node.children.pop()
+                    temp_node.generate_children(candidates)
+            else:  # 说明该节点没有一个合法的子节点，需要向上回溯
+                temp_node = temp_node.parent
+        result = set()
+        for item in temp_result:
+            item.sort()
+            result.add(tuple(item))
+        return [list(item) for item in result]
 
-            pass
-    pass
+
+candidates = [92,71,89,74,102,91,70,119,86,116,114,106,80,81,115,99,117,93,76,77,111,110,75,104,95,112,94,73]
+target = 310
+s = Solution()
+print(s.combinationSum(candidates, target))

88269ae70d985d80d9ef5228bde2685f8dca10d5
YinongLong <pleasecallmegeek@gmail.com>
2017-07-24 20:29:16 +0800

    backtracking

---
+++ leetcode/misc/exercise_39.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+class Solution(object):
+
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        
+        pass
+    pass

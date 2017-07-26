af4fc1809f5c301defbbccc75b697fa565a063fa
YinongLong <pleasecallmegeek@gmail.com>
2017-07-26 23:18:51 +0800

    update

--- leetcode/array/exercise_43.py
+++ leetcode/array/exercise_43.py
@@ -11,4 +11,16 @@ class Solution(object):
         """
         num1 = list(num1)
         num2 = list(num2)
+        result = []
+        for i in range(-1, -len(num2)-1, -1):
+            temp_result = []
+            location = None
+            temp_num2 = int(num2[i])
+            for j in range(-1, -len(num1)-1, -1):
+                temp_num1 = int(num1[j])
+                product = temp_num1 * temp_num2
+                if location is None:
+                    temp_result.append(product % 10)
+                    
+            pass
         pass

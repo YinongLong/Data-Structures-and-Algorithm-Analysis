99403c6135ae16ff40d865aaa9b119f9c879f15d
YinongLong <pleasecallmegeek@gmail.com>
2017-07-26 23:11:36 +0800

    take a way of using list to solve the problem

---
+++ leetcode/array/exercise_43.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+class Solution(object):
+
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        num1 = list(num1)
+        num2 = list(num2)
+        pass

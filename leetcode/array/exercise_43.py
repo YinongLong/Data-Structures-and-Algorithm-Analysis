1b6ab01963588b06f17e81d7dfe252048a30d484
YinongLong <pleasecallmegeek@gmail.com>
2017-07-28 22:25:02 +0800

    big number multiply

--- leetcode/array/exercise_43.py
+++ leetcode/array/exercise_43.py
@@ -3,6 +3,12 @@ from __future__ import print_function
 
 class Solution(object):
 
+    def addition(self, nums_list, idx, num):
+        nums_list[idx] += num
+        if nums_list[idx] >= 10:
+            nums_list[idx] %= 10
+            nums_list[idx-1] += 1
+
     def multiply(self, num1, num2):
         """
         :type num1: str
@@ -11,16 +17,32 @@ class Solution(object):
         """
         num1 = list(num1)
         num2 = list(num2)
-        result = []
-        for i in range(-1, -len(num2)-1, -1):
-            temp_result = []
-            location = None
+        len_num1 = len(num1)
+        len_num2 = len(num2)
+        result = [0] * (len_num1 + 1 + len_num2 + 1 - 1)
+        for i in range(-1, -len_num2 - 1, -1):
+            temp_result = [0] * (len_num1 + 1)
             temp_num2 = int(num2[i])
-            for j in range(-1, -len(num1)-1, -1):
+            for j in range(-1, -len_num1 - 1, -1):
                 temp_num1 = int(num1[j])
                 product = temp_num1 * temp_num2
-                if location is None:
-                    temp_result.append(product % 10)
-                    
-            pass
-        pass
+                self.addition(temp_result, j, product % 10)
+                self.addition(temp_result, j-1, product / 10)
+            for k in range(-1, -len(temp_result)-1, -1):
+                self.addition(result, k + i + 1, temp_result[k])
+        str_result = []
+        starting = False
+        for item in result:
+            if starting:
+                str_result.append(str(item))
+            else:
+                if item != 0:
+                    starting = True
+                    str_result.append(str(item))
+        return ''.join(str_result)
+
+
+num1 = '758'
+num2 = '234'
+s = Solution()
+print(s.multiply(num1, num2))

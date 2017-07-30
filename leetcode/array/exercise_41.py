386d9501b751c576d4d6322e4a176bf9abc07a1a
YinongLong <pleasecallmegeek@gmail.com>
2017-07-30 21:21:45 +0800

    a problem about array

---
+++ leetcode/array/exercise_41.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+class Solution(object):
+
+    def firstMissingPositive(self, nums):
+        """
+        :types nums: List[int]
+        :rtype: int
+        """
+        
+        pass

052920be9b49e182452bd30bb55aaffe1b5ddedd
YinongLong <pleasecallmegeek@gmail.com>
2017-07-30 21:39:40 +0800

    a methodological analysis of solving problem method

--- leetcode/array/exercise_41.py
+++ leetcode/array/exercise_41.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
+from __future__ import print_function
 
 class Solution(object):
 
     def firstMissingPositive(self, nums):
         """
+        这是一个关于Array的练习，要求时间复杂度为O(n)，并且空间复杂度为常数。
+        有一个方法论就是尽可能的使用题目中掌握的所有信息，这样才有更大的可能性找出
+        比较满意的解决方案。在关于这个题目解决的过程中，关于Array的题目，我们很容易
+        就发现有一个index的信息我们是可以利用的。
         :types nums: List[int]
         :rtype: int
         """
-        
+        print()
         pass

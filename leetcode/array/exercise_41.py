6db520053b6bdb0cbaf3a2b152e3d144e2287f19
YinongLong <pleasecallmegeek@gmail.com>
2017-07-30 22:17:19 +0800

    a complete solving method

--- leetcode/array/exercise_41.py
+++ leetcode/array/exercise_41.py
@@ -12,5 +12,26 @@ class Solution(object):
         :types nums: List[int]
         :rtype: int
         """
-        print()
-        pass
+        len_nums = len(nums)
+        if len_nums == 1:
+            return 1 if nums[0] != 1 else 2
+        for i in range(len_nums):
+            cur_idx = i
+            temp_num = nums[cur_idx]
+            if temp_num <= 0:
+                nums[cur_idx] = -1  # 用-1代表该索引位置待填充
+                continue
+            elif (temp_num - 1) == cur_idx:  # 代表当前索引位与数字对齐
+                continue
+            else:  # 代表当前索引位置的数字与索引没有对齐，则对数字代表的索引进行追踪，直到出现负数或对齐
+                nums[cur_idx] = -1
+                while (temp_num - 1) != cur_idx and temp_num > 0 and temp_num <= len_nums:
+                    cur_idx = temp_num
+                    temp_num = nums[cur_idx]
+                    # 因为是通过索引找过来的，所以该位置应该对齐
+                    nums[cur_idx] = cur_idx + 1
+
+        for i in range(1, len_nums):
+            if (nums[i] - 1) != i:
+                return i + 1
+        return len_nums + 1

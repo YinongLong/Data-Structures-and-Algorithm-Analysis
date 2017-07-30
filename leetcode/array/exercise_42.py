52b46613843c5c42021d50b9d2f40e62229300dd
YinongLong <pleasecallmegeek@gmail.com>
2017-07-30 23:37:24 +0800

    thinking

---
+++ leetcode/array/exercise_42.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+
+class Solution(object):
+
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+
+        

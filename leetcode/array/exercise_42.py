c62648befa12afd4bf64babcd19bcd967d77d3dc
YinongLong <pleasecallmegeek@gmail.com>
2017-08-05 23:15:24 +0800

    the brute-force algorithm finish.

--- leetcode/array/exercise_42.py
+++ leetcode/array/exercise_42.py
@@ -16,12 +16,23 @@ class Solution(object):
         while start_elevation == 0 and start_idx < length:
             start_idx += 1
             start_elevation = height[start_idx]
+
+        trapping_water = 0
         if start_idx < length - 2:  # we can trap rain water at least three bar
-            
+            container = []
             for i in range(start_idx + 1, length):
                 temp_elevation = height[i]
-
-                pass
-            pass
-        else:
-            return 0
+                if temp_elevation < start_elevation:
+                    container.append(start_elevation - temp_elevation)
+                else:
+                    trapping_water += sum(container)
+                    start_elevation = temp_elevation
+                    container = []
+            if len(container) > 1:
+                end_elevation = container.pop()
+                while len(container) > 0:
+                    temp_elevation = container.pop()
+                    residual = temp_elevation - end_elevation
+                    if residual > 0:
+                        trapping_water += residual
+        return trapping_water

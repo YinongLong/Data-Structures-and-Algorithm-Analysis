22e3f017e5c513b54a6f7b22c460befb50a692f1
YinongLong <pleasecallmegeek@gmail.com>
2017-08-05 23:03:43 +0800

    try using brute-force algorithm to solve this problem

--- leetcode/array/exercise_42.py
+++ leetcode/array/exercise_42.py
@@ -9,5 +9,19 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
+        length = len(height)
+        # find the elevation, which is greater than 0
+        start_idx = 0
+        start_elevation = height[start_idx]
+        while start_elevation == 0 and start_idx < length:
+            start_idx += 1
+            start_elevation = height[start_idx]
+        if start_idx < length - 2:  # we can trap rain water at least three bar
+            
+            for i in range(start_idx + 1, length):
+                temp_elevation = height[i]
 
-        
+                pass
+            pass
+        else:
+            return 0

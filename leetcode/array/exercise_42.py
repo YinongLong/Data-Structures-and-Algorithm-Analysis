722b0c8c722d20e120c7fac8b95ad164b26369f1
YinongLong <pleasecallmegeek@gmail.com>
2017-08-09 23:42:30 +0800

    try using a type of two pointers method

--- leetcode/array/exercise_42.py
+++ leetcode/array/exercise_42.py
@@ -4,6 +4,71 @@ from __future__ import print_function
 
 class Solution(object):
 
+    def trap_two_pointers(self, height):
+        if not height:  return 0
+
+        left_pointer = 0
+        right_pointer = len(height) - 1
+        while height[left_pointer] == 0:
+            left_pointer += 1
+            if left_pointer >= len(height): break
+        while height[right_pointer] == 0:
+            right_pointer -= 1
+            if right_pointer < 0: break
+        trapping_water = 0
+        if (left_pointer < len(height)) and \
+            (right_pointer > 0):
+            left_max_elevation = height[left_pointer]
+            right_max_elevation = height[right_pointer]
+        left_container = []
+        right_container = []
+        while left_pointer < right_pointer:
+            # firstly, move the left pointer
+            left_pointer += 1
+            if left_pointer == right_pointer:
+                left_sum = sum(left_container)
+                right_sum = sum(right_container)
+                max_diff = left_max_elevation - right_max_elevation
+                if max_diff > 0:
+                    left_sum -= max_diff * len(left_container)
+                if max_diff < 0:
+                    right_sum += max_diff * len(right_container)
+                left_container = []
+                right_container = []
+                trapping_water += left_sum + right_sum
+                break
+
+            if height[left_pointer] >= left_max_elevation:
+                trapping_water += sum(left_container)
+                left_container = []
+                left_max_elevation = height[left_pointer]
+            else:
+                left_container.append(left_max_elevation - height[left_pointer])
+
+            # secondly, move the right pointer
+            right_pointer -= 1
+            if right_pointer == left_pointer:
+                left_sum = sum(left_container)
+                right_sum = sum(right_container)
+                max_diff = left_max_elevation - right_max_elevation
+                if max_diff > 0:
+                    left_sum -= max_diff * len(left_container)
+                    left_sum = 0 if left_sum < 0 else left_sum
+                if max_diff < 0:
+                    right_sum += max_diff * len(right_container)
+                    right_sum = 0 if right_sum < 0 else right_sum
+                left_container = []
+                right_container = []
+                trapping_water += left_sum + right_sum
+                break
+            if height[right_pointer] >= right_max_elevation:
+                trapping_water += sum(right_container)
+                right_container = []
+                right_max_elevation = height[right_pointer]
+            else:
+                right_container.append(right_max_elevation - height[right_pointer])
+        return trapping_water
+
     def trap(self, height):
         """
         :type height: List[int]
@@ -41,4 +106,4 @@ class Solution(object):
 
 height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
 s = Solution()
-print(s.trap(height))
+print(s.trap_two_pointers([5, 4, 1, 2]))

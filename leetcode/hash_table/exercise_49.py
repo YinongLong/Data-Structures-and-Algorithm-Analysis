da5b83e6daa4f65c42e775ddde4fd7e30515c1e0
YinongLong <pleasecallmegeek@gmail.com>
2017-08-18 23:06:24 +0800

    i am considering which method can solve this problem

---
+++ leetcode/hash_table/exercise_49.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+class Solution(object):
+
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        
+        pass

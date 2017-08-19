ba2d2ed2810143a67e2909debca158914c6e1dc2
YinongLong <pleasecallmegeek@gmail.com>
2017-08-19 10:09:47 +0800

    Firstly sort the word by char, then insert them into a hashtable

--- leetcode/hash_table/exercise_49.py
+++ leetcode/hash_table/exercise_49.py
@@ -3,10 +3,30 @@ from __future__ import print_function
 
 class Solution(object):
 
+    def _update_records(self, records, word, idx):
+        chars = list(word)
+        chars.sort()
+        identity = ''.join(chars)
+        container = records.get(identity, [])
+        container.append(idx)
+        records[identity] = container
+
     def groupAnagrams(self, strs):
         """
         :type strs: List[str]
         :rtype: List[List[str]]
         """
-        
-        pass
+        records = dict()
+        for idx, word in enumerate(strs):
+            self._update_records(records, word, idx)
+        result = []
+        for idx_list in records.values():
+            result.append([])
+            for idx in idx_list:
+                result[-1].append(strs[idx])
+        return result
+
+
+strs = ['eat', 'ate', 'tea', 'tan', 'nat', 'bat']
+s = Solution()
+print(s.groupAnagrams(strs))

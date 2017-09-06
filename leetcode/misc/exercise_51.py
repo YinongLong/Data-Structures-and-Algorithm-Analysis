dc09581f9bb4a042113a6ea389711a0a41610cf7
YinongLong <pleasecallmegeek@gmail.com>
2017-09-06 22:26:02 +0800

    update some details of the method

--- leetcode/misc/exercise_51.py
+++ leetcode/misc/exercise_51.py
@@ -13,7 +13,7 @@ from __future__ import print_function
 
 class Node(object):
 
-    def __init__(self, x, y, parent, positions, level):
+    def __init__(self, path, parent, positions, level):
         """
         :type x: int
             生成该结点的x坐标
@@ -26,29 +26,16 @@ class Node(object):
         :type level: int
             记录该结点在树中的高度，代表已经在棋盘上放置的棋子的数目
         """
-        self.x = x
-        self.y = y
-        self.have_children = False
+        self.path = path
         self.parent = parent
         self.positions = positions
         self.level = level
-        if self.positions:
-            self.have_children = True
-
-    def empty(self):
-        """
-        返回时候该结点还存在可以用来生成孩子结点的有效位置
-        """
-        return not self.have_children
 
     def next(self):
         """
         生成下一个用于生成孩子结点的位置以及有效的放置棋子的位置
         """
         x, y = self.positions.pop()
-        if len(self.positions) == 0:
-            self.have_children = False
-
         children = []
         for temp_x, temp_y in self.positions:
             if temp_x == x or temp_y == y:
@@ -61,16 +48,6 @@ class Node(object):
 
 class Solution(object):
 
-    def generate_solution(self, tree_node):
-        """
-        根据一个搜索树的叶子结点生成一个解法的放置棋子的位置元组列表
-        """
-        solution = []
-        while tree_node.parent:
-            solution.append((tree_node.x, tree_node.y))
-            tree_node = tree_node.parent
-        return solution
-
     def solveNQueens(self, n):
         """
         :type n: int
@@ -82,19 +59,19 @@ class Solution(object):
             for j in range(n):
                 positions.append((i, j))
         # 生成树的根结点
-        root = Node(None, None, None, positions, 0)
+        root = Node([], None, positions, 0)
         temp_node = root
         solutions = []
         while temp_node:
             if temp_node.level == n:  # 说明n个位置都已经放置棋子，则返回结果
-                solutions.append(self.generate_solution(temp_node))
+                solutions.append(temp_node.path)
                 temp_node = temp_node.parent
                 continue
-            if temp_node.empty():  # 代表该结点不能再生成子结点
+            if len(temp_node.positions) == 0:  # 代表该结点不能再生成子结点
                 temp_node = temp_node.parent
             else:  # 代表该结点还可以生成子结点
                 (next_x, next_y), children = temp_node.next()
-                temp_node = Node(next_x, next_y, temp_node, children, temp_node.level + 1)
+                temp_node = Node(temp_node.path + [(next_x, next_y)], temp_node, children, temp_node.level + 1)
 
         result = []
         for solution in solutions:
@@ -107,4 +84,4 @@ class Solution(object):
 
 
 s = Solution()
-print(s.solveNQueens(8))
+print(s.solveNQueens(9))

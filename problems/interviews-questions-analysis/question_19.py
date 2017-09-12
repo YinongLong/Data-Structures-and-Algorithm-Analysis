40cef856850b9ff2a8735ff035117c62454d4cee
YinongLong <pleasecallmegeek@gmail.com>
2017-09-12 23:14:56 +0800

    utilize partition method of quick sort to solve this problem

---
+++ problems/interviews-questions-analysis/question_19.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+class Solution(object):
+
+    def reOrderArray(self, array):
+        start = -1
+        for idx, item in enumerate(array):
+            if item % 2 == 1:
+                start += 1
+            else:
+                
+                pass
+            pass
+        pass

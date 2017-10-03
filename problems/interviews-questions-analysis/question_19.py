b58ecf15cf2d2c0ed171b4bbee4bfe1081a46775
YinongLong <pleasecallmegeek@gmail.com>
2017-10-03 23:01:48 +0800

    utilize insert sort idea

--- problems/interviews-questions-analysis/question_19.py
+++ problems/interviews-questions-analysis/question_19.py
@@ -4,14 +4,24 @@ from __future__ import print_function
 class Solution(object):
 
     def reOrderArray(self, array):
-        start = -1
-        for idx, item in enumerate(array):
-            if item % 2 == 1:
-                start += 1
-                if start != idx:
-                    array[start], array[idx] = array[idx], array[start]
+        length = len(array)
+        for i in range(1, length):
+            temp_num = array[i]
+            if temp_num % 2 == 0:
+                continue
+            for j in range(i - 1, -1, -1):
+                if array[j] % 2 == 0:
+                    array[j+1] = array[j]
+                else:
+                    array[j+1] = temp_num
+                    break
+            else:
+                array[0] = temp_num
+
+        return array
+
 
 s = Solution()
-array = [1,2,3,4,5,6,7]
+array = [2,4,6,1,3,5,7]
 s.reOrderArray(array)
 print(array)
